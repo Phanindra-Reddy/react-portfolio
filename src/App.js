afc0f006fd3fe0813551fa8da3eb00c652552c0c
Phanindra-Reddy <mpr4070@gmail.com>
2021-05-23 10:34:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import About from './Components/About/About';
 import NavBar from './Components/NavBar/NavBar';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
-import Projects from './Components/Projects/Projects';
-import Resources from './Components/Resources/Resources';
 
 function App() {
   return (
@@ -16,8 +14,6 @@ function App() {
       <Home />
       <NavBar />
       <About />
-      <Projects />
-      <Resources />
       <Contact />
       <Footer/>
 
