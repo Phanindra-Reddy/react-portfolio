3915079c9c0b15b0c032305525e8f8388559838c
Phanindra-Reddy <mpr4070@gmail.com>
2021-05-23 10:41:52 +0530

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from './Components/About/About';
 import NavBar from './Components/NavBar/NavBar';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
+import Projects from './Components/Projects/Projects';
+import Resources from './Components/Resources/Resources';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Home />
       <NavBar />
       <About />
+      <Projects/>
+      <Resources />
       <Contact />
       <Footer/>
 
